3a8e0a7b5f3dd522205e5460127c59dfaaa8e6f2
KhatriSuson <susonkhatri110@gmail.com>
2024-09-12 12:17:08 +0545

    sum_of_prime

--- sample_program1/b_sum_of_prime_in_range.py
+++ sample_program1/b_sum_of_prime_in_range.py
@@ -1,1 +1,23 @@
-def prime()
+import math
+def prime(n):
+    if n <= 1:
+        return False
+    
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def sum_of_prime(start, end):
+    total = 0
+    for num in range(start, end + 1):
+        if prime(num):
+            total += num
+    return total
+
+start = 10
+end = 50
+result = sum_of_prime(start, end)
+
+print(f"Sum of prime number is {start} and {end}: {result}")
+
